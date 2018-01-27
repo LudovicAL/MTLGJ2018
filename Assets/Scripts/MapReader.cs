e67f54664c17577e4f116540b22aa0a2559edcae
alechevalier1990 <aurelielechevalier@hotmail.com>
2018-01-27 14:10:37 -0500

    First pass on bitmap. Read from black and white texture to extract array of collisions, then be able to convert in-world position to pixel position.

---
+++ Assets/Scripts/MapReader.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MapReader : MonoBehaviour {
+	bool[,] m_Bitmap;
+	int m_Width;
+	int m_Height;
+
+	void Start() {
+		string filePath = Application.dataPath + "/Arts/ProtoCity_01.png";
+		Texture2D tex = LoadPNG (filePath);
+
+		if (tex == null) {
+			Debug.Log ("Could not find texture, will not load map.");
+			return;
+		}
+
+		m_Width = tex.width;
+		m_Height = tex.height;
+		m_Bitmap = new bool[m_Width,m_Height];
+
+		for (int i = 0; i < m_Width; ++i) {
+			for (int j = 0; j < m_Height; ++j) {
+				Color color = tex.GetPixel (i, j);
+				m_Bitmap [i,j] = color == Color.white ? true : false;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool CanMoveThere(float _xPos, float _yPos)
+	{
+		int pixelPosX = (int)((m_Width / 2.0f) + _xPos*100.0f);
+		int pixelPosY = (int)((m_Height / 2.0f) + _yPos*100.0f); 
+
+		//Debug.Log ("Got " + _xPos + ", " + _yPos + " converted to pixels " + pixelPosX + ", " + pixelPosY);
+
+		if (pixelPosX < 0 || pixelPosX >= m_Width)
+			return false;
+
+		if (pixelPosY < 0 || pixelPosY >= m_Height)
+			return false;
+
+		return m_Bitmap[pixelPosX, pixelPosY];
+	}
+
+	public Texture2D LoadPNG(string _filePath)
+	{
+		Texture2D tex = null;
+		byte[] fileData;
+
+		if (File.Exists(_filePath))     {
+			fileData = File.ReadAllBytes(_filePath);
+			tex = new Texture2D(2, 2);
+			tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+		}
+		return tex;
+	}
+
+}
---
+++ Assets/Scripts/TestMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour {
+	public float movementSpeed = 0.06f;
+	MapReader g_MapReader;
+
+	// Use this for initialization
+	void Start () {
+		g_MapReader = GameObject.Find ("MapReader").GetComponent<MapReader> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3 (0.0f, 0.0f, 0.0f);
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			movement += new Vector3 (-1.0f, 0.0f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			movement += new Vector3 (1.0f, 0.0f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			movement += new Vector3 (0.0f, 1.0f, 0.0f);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			movement += new Vector3 (0.0f, -1.0f, 0.0f);
+		}
+
+		if (movement == Vector3.zero)
+			return;
+
+		Vector3 wantedPos = transform.position + movement * movementSpeed;
+		if (g_MapReader.CanMoveThere(wantedPos[0], wantedPos[1]))
+			transform.position = wantedPos;
+	}
+}
