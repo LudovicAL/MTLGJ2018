fe0b42c1e33b7b494059d73580e697c2c1441d8b
DESKTOP-3QGJ82B\Dudu <ducharme.seb@gmail.com>
2018-01-27 17:12:06 -0500

    Push 20180127 17h12

--- Assets/Scripts/ColorChange.cs
+++ Assets/Scripts/ColorChange.cs
@@ -6,6 +6,8 @@ using System;
 
 public class ColorChange : MonoBehaviour
 {
+    
+
 
     List<Vector2> GridList = new List<Vector2>();
     Color CurrentColor;
@@ -14,67 +16,87 @@ public class ColorChange : MonoBehaviour
     float InfectedSpeed = 3f;
     float CivilianSPeed = 1.5f;
     public Transform Civilian;
-
-	private MapReader g_MapReader;
+    private MapReader g_MapReader;
 
     // Use this for initialization
     void Start()
     {
-		g_MapReader = GameObject.Find ("MapReader").GetComponent<MapReader> ();
-
         float yAxis = Civilian.position.y;
         float xAxis = Civilian.position.x;
+        m_GridOffset = -1.0f * new Vector3((m_GridWidth * 0.5f * m_GridCellWidthHeight), (m_GridHeight * 0.5f * m_GridCellWidthHeight), 0.0f);
 
-        GridSystem();
-
-        for (int i = 0; i < 1000; i++)
+        if (GameObject.Find("Map") != null)
         {
-			if (g_MapReader != null) { // valid map spawn
-				float[] randomSpawnPos = g_MapReader.FindRandomWhiteSpace ();
-				xAxis = randomSpawnPos [0];
-				yAxis = randomSpawnPos [1];
-			} else { // test spawn
-				xAxis += 2;
-				if (xAxis >= 35) {
-					xAxis = Civilian.position.x;
-					yAxis += 2;
-				}
-			}
-
-			Transform newCivilian = Instantiate(Civilian, new Vector3(xAxis, yAxis, 0), Quaternion.identity);
-            newCivilian.tag = "Civilian";
-            newCivilian.name = "Civilian" + i;
-            GridPosition(newCivilian);
+            g_MapReader = GameObject.Find("Map").GetComponent<MapReader>();
         }
 
-        
+        for (int i = 0; i < 100; i++)
+        {
+            if (g_MapReader != null)
+            { // valid map spawn
+                float[] randomSpawnPos = g_MapReader.FindRandomWhiteSpace();
+                xAxis = randomSpawnPos[0];
+                yAxis = randomSpawnPos[1];
+            }
+            else
+            { // test spawn
+                xAxis += 2;
+                if (xAxis >= 35)
+                {
+                    xAxis = Civilian.position.x;
+                    yAxis += 2;
+                }
+            }
+
+
+
+            Transform newCivilian = Instantiate(Civilian, new Vector3(xAxis, yAxis, 0), Quaternion.identity);
+            newCivilian.tag = "Civilian";
+        }
+
+
+        BuildInitialListOfCivilians();
+        SetupGameObjectGridList();
+        InitialInfect();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        DrawDebugGrid();
-        //string[] ActiveChar = new string[] { "Civilian", "Infected" };
-        //for (int i = 0; i <= 1; i++)
-        //{
-        //    GameObject[] objects = GameObject.FindGameObjectsWithTag(ActiveChar[i]);
-        //    foreach (GameObject obj in objects)
-        //    { 
-        //        switch (i)
-        //        {
-        //            case 0:
+        // DrawDebugGrid();
+        UpdateCivilianGameObjectLists();
+        //Test_DrawCivilianGameObjectListDebugLinesStaggered();
 
-        //                break;
-        //            case 1:
-        //                ZombieDecay(obj);
-        //                GameObject target = GetClosestCivilian(obj);
-        //                if (target != null && IsHungry(obj)) { RushCivilian(obj, target); }
+        int amountOfCivilians = m_Civilians.Length;
+        for (int i = 0; i < amountOfCivilians; ++i)
+        {
 
-        //                break;
-        //        }
-        //    }
-        //}
+
+            switch (m_Civilians[i].tag)
+            {
+                case "Civilian":
+
+                    break;
+                case "Infected":
+                    ZombieDecay(m_Civilians[i]);
+                    GameObject target = GetClosestCivilian(m_Civilians[i], i);
+                    if (IsHungry(m_Civilians[i]))
+                    {
+                        if (target != null)
+                        {
+                            RushCivilian(m_Civilians[i], target);
+                        }
+                        else
+                        {
+                            MoveRandomly(m_Civilians[i]);
+                        }
+                    }
+
+                    break;
+            }
+
+        }
 
     }
 
@@ -90,7 +112,7 @@ public class ColorChange : MonoBehaviour
         Red = ActiveSprite.color.r;
         Blue = ActiveSprite.color.b;
 
-        if (ActiveSprite.color.g <= 0f)
+        if (ActiveSprite.color.g <= .1f)
         {
             ActiveSprite.tag = "Dead";
         }
@@ -125,20 +147,24 @@ public class ColorChange : MonoBehaviour
 
     }
 
-    GameObject GetClosestCivilian(GameObject ActiveInfected)
+    GameObject GetClosestCivilian(GameObject ActiveInfected, int i)
     {
+
         GameObject bestTarget = null;
         float ClosestDist = Mathf.Infinity;
         Vector3 currentPosition = ActiveInfected.transform.position;
 
-        GameObject[] objects = GameObject.FindGameObjectsWithTag("Civilian");
-        foreach (var obj in objects)
+
+        foreach (var obj in GameObjectGridList[m_CivilianGridIndex[i]])
         {
-            float TargetDist = Vector3.Distance(currentPosition, obj.transform.position);
-            if (TargetDist < ClosestDist)
+            if (obj.tag != "Infected")
             {
-                ClosestDist = TargetDist;
-                bestTarget = obj;
+                float TargetDist = Vector3.Distance(currentPosition, obj.transform.position);
+                if (TargetDist < ClosestDist)
+                {
+                    ClosestDist = TargetDist;
+                    bestTarget = obj;
+                }
             }
         }
 
@@ -163,8 +189,15 @@ public class ColorChange : MonoBehaviour
                 targetPosition,
                 InfectedSpeed * Time.deltaTime);
         }
+    }
 
-
+    void MoveRandomly(GameObject ActiveInfected)
+    {
+        ActiveInfected.transform.position =
+            Vector3.MoveTowards(
+            ActiveInfected.transform.position,
+            ActiveInfected.transform.position + new Vector3(UnityEngine.Random.Range(-1.0f, 1.0f), UnityEngine.Random.Range(-1.0f, 1.0f), 0.0f),
+                InfectedSpeed * Time.deltaTime * 3.0f);
     }
 
     bool IsHungry(GameObject ActiveInfected)
@@ -175,46 +208,109 @@ public class ColorChange : MonoBehaviour
         return false;
     }
 
-    int m_GridWidth = 20;
-    int m_GridHeight = 20;
-    float m_GridCellWidthHeight = 1.0f;
-    Vector3 m_Offset;
+    int m_GridWidth = 50;
+    int m_GridHeight = 50;
+    float m_GridCellWidthHeight = 8.0f;
+    Vector3 m_GridOffset;
+
+    List<List<GameObject>> GameObjectGridList = new List<List<GameObject>>();
+    GameObject[] m_Civilians;
+    int[] m_CivilianGridIndex;
+
+    void BuildInitialListOfCivilians()
+    {
+        m_Civilians = GameObject.FindGameObjectsWithTag("Civilian");
+
+        int amountOfCivilians = m_Civilians.Length;
+        m_CivilianGridIndex = new int[amountOfCivilians];
+
+        for (int i = 0; i < amountOfCivilians; ++i)
+        {
+            m_CivilianGridIndex[i] = 0;
+        }
+
+    }
+
+    void SetupGameObjectGridList()
+    {
+        for (int i = 0; i < m_GridWidth * m_GridHeight; ++i)
+        {
+            GameObjectGridList.Add(new List<GameObject>());
+        }
+    }
 
     void DrawDebugGrid()
     {
-        m_Offset = -1.0f * new Vector3((m_GridWidth * 0.0f * m_GridCellWidthHeight), (m_GridHeight * 0.0f * m_GridCellWidthHeight), 0.0f);
+
         for (int i = 0; i <= m_GridWidth; i++)
         {
-            Debug.DrawLine(new Vector3((i * m_GridWidth * m_GridCellWidthHeight), 0.0f, 0.0f) + m_Offset, new Vector3((i * m_GridWidth * m_GridCellWidthHeight), m_GridHeight * m_GridCellWidthHeight, 0.0f) + m_Offset, Color.black);
+            Debug.DrawLine(new Vector3((i * m_GridCellWidthHeight), 0.0f, 0.0f) + m_GridOffset, new Vector3((i * m_GridCellWidthHeight), m_GridHeight * m_GridCellWidthHeight, 0.0f) + m_GridOffset, Color.black);
         }
 
         for (int i = 0; i <= m_GridHeight; i++)
         {
-            Debug.DrawLine(new Vector3(0.0f, (i * m_GridHeight * m_GridCellWidthHeight), 0.0f) + m_Offset, new Vector3(m_GridWidth * m_GridCellWidthHeight * m_GridCellWidthHeight, (i * m_GridHeight), 0.0f) + m_Offset, Color.black);
+            Debug.DrawLine(new Vector3(0.0f, (i * m_GridCellWidthHeight), 0.0f) + m_GridOffset, new Vector3(m_GridWidth * m_GridCellWidthHeight, (i * m_GridCellWidthHeight), 0.0f) + m_GridOffset, Color.black);
         }
-
     }
 
-    void GridSystem()
+    void UpdateCivilianGameObjectLists()
     {
-        int xPos;
-        int yPos;
-        
-        for (int x = 0; x <= 20; x++)
+        int amountOfCivilians = m_Civilians.Length;
+        for (int i = 0; i < amountOfCivilians; ++i)
         {
-            for (int y = 0; y <= 20; y++)
+            int newCivilianGridIndex = GetIndexFromGridCoordinates(GetGridXCoordinateFromWorldPosition(m_Civilians[i].transform.position.x), GetGridYCoordinateFromWorldPosition(m_Civilians[i].transform.position.y));
+            if (m_CivilianGridIndex[i] != newCivilianGridIndex)
             {
-                xPos = -100 + (x * 20);
-                yPos = -100 + (y * 20);
-                GridList.Add( new Vector2(xPos, yPos) );
+                //Unregister civilian from old grid group
+                GameObjectGridList[m_CivilianGridIndex[i]].Remove(m_Civilians[i]);
+                //Register civilian to new grid group
+                GameObjectGridList[newCivilianGridIndex].Add(m_Civilians[i]);
+
+                m_CivilianGridIndex[i] = newCivilianGridIndex;
             }
         }
-
-        
     }
 
-    void GridPosition(Transform ActiveCivilian)
+    void Test_DrawCivilianGameObjectListDebugLinesStaggered()
     {
-        
+        int amountOfCivilians = m_Civilians.Length;
+        for (int i = 0; i < amountOfCivilians; ++i)
+        {
+            Debug.DrawLine(m_Civilians[i].transform.position, GetGridSquarePositionFromIndex(m_CivilianGridIndex[i]), Color.black);
+        }
+    }
+
+    int GetIndexFromGridCoordinates(int x, int y)
+    {
+        return (x + (y * m_GridWidth));
+    }
+
+    int GetGridXCoordinateFromWorldPosition(float xPos)
+    {
+        return (int)((xPos - m_GridOffset.x) / m_GridCellWidthHeight);
+    }
+
+    int GetGridYCoordinateFromWorldPosition(float yPos)
+    {
+        return (int)((yPos - m_GridOffset.y) / m_GridCellWidthHeight);
+    }
+
+    Vector3 GetGridSquarePositionFromIndex(int index)
+    {
+        int y = index / m_GridHeight;
+        int x = index - (m_GridHeight * y);
+
+        return new Vector3(x * m_GridCellWidthHeight, y * m_GridCellWidthHeight, 0.0f) + m_GridOffset + new Vector3(m_GridCellWidthHeight * 0.5f, m_GridCellWidthHeight * 0.5f);
+    }
+
+
+    void InitialInfect()
+    {
+
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Civilian");
+        int index = objects.Length;
+        int randomCivilian = UnityEngine.Random.Range(0, index);
+        objects[randomCivilian].tag = "Infected";
+
     }
 }
