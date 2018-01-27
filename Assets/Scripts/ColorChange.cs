4166839177e82c91fdb20e51dc27604c1c67f554
DESKTOP-3QGJ82B\Dudu <ducharme.seb@gmail.com>
2018-01-27 14:29:21 -0500

    BECAUSE AURELIE

--- Assets/Scripts/ColorChange.cs
+++ Assets/Scripts/ColorChange.cs
@@ -3,51 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+
 public class ColorChange : MonoBehaviour
 {
 
-    
+    List<Vector2> GridList = new List<Vector2>();
     Color CurrentColor;
     Color NewColor;
     float Red, Blue, Green;
-    public Transform Civilian;
     float InfectedSpeed = 3f;
     float CivilianSPeed = 1.5f;
+    public Transform Civilian;
 
     // Use this for initialization
     void Start()
     {
-        //for (int i = 0; i < 10; i++)
-        //{
-        //    Transform newCivilian = Instantiate(Civilian, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
-        //    newCivilian.tag = "Civilian";
-        //}
+        float yAxis = Civilian.position.y;
+        float xAxis = Civilian.position.x;
+
+        GridSystem();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            xAxis += 2;
+            if (xAxis >= 35)
+            {
+                xAxis = Civilian.position.x;
+                yAxis += 2;
+            }
+
+
+            Transform newCivilian = Instantiate(Civilian, new Vector3(xAxis, yAxis, 0), Quaternion.identity);
+            newCivilian.tag = "Civilian";
+            newCivilian.name = "Civilian" + i;
+            GridPosition(newCivilian);
+        }
+
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        DrawDebugGrid();
+        //string[] ActiveChar = new string[] { "Civilian", "Infected" };
+        //for (int i = 0; i <= 1; i++)
+        //{
+        //    GameObject[] objects = GameObject.FindGameObjectsWithTag(ActiveChar[i]);
+        //    foreach (GameObject obj in objects)
+        //    { 
+        //        switch (i)
+        //        {
+        //            case 0:
 
-        string[] ActiveChar = new string[] { "Civilian", "Infected" };
-        for (int i = 0; i <= 1; i++)
-        {
-            GameObject[] objects = GameObject.FindGameObjectsWithTag(ActiveChar[i]);
-            foreach (GameObject obj in objects)
-            { 
-                switch (i)
-                {
-                    case 0:
-                       
-                        break;
-                    case 1:
-                        ZombieDecay(obj);
-                        GameObject target = GetClosestCivilian(obj);
-                        if (target != null && IsHungry(obj)) { RushCivilian(obj, target); }
-                        
-                        break;
-                }
-            }
-        }
+        //                break;
+        //            case 1:
+        //                ZombieDecay(obj);
+        //                GameObject target = GetClosestCivilian(obj);
+        //                if (target != null && IsHungry(obj)) { RushCivilian(obj, target); }
+
+        //                break;
+        //        }
+        //    }
+        //}
 
     }
 
@@ -62,8 +81,6 @@ public class ColorChange : MonoBehaviour
 
         Red = ActiveSprite.color.r;
         Blue = ActiveSprite.color.b;
-
-        print("Red : " + Red + "  Green : " + ActiveSprite.color.g + "  Blue : " + Blue);
 
         if (ActiveSprite.color.g <= 0f)
         {
@@ -97,7 +114,7 @@ public class ColorChange : MonoBehaviour
             //print("Red : " + Red + "  Green : " + ActiveSprite.color.g + "  Blue : " + Blue);
             ActiveSprite.color = NewColor;
         }
-        
+
     }
 
     GameObject GetClosestCivilian(GameObject ActiveInfected)
@@ -116,21 +133,19 @@ public class ColorChange : MonoBehaviour
                 bestTarget = obj;
             }
         }
-        
+
         return bestTarget;
-        
+
     }
 
     void RushCivilian(GameObject ActiveInfected, GameObject Target)
     {
         Vector3 targetPosition = Target.transform.position;
 
-        if (Vector3.Distance(ActiveInfected.transform.position,Target.transform.position) <= 1f)
+        if (Vector3.Distance(ActiveInfected.transform.position, Target.transform.position) <= 1f)
         {
-            
+
             Target.tag = "Infected";
-            print(Target.name);
-            print(Target.name);
         }
         else
         {
@@ -152,4 +167,46 @@ public class ColorChange : MonoBehaviour
         return false;
     }
 
+    int m_GridWidth = 20;
+    int m_GridHeight = 20;
+    float m_GridCellWidthHeight = 1.0f;
+    Vector3 m_Offset;
+
+    void DrawDebugGrid()
+    {
+        m_Offset = -1.0f * new Vector3((m_GridWidth * 0.0f * m_GridCellWidthHeight), (m_GridHeight * 0.0f * m_GridCellWidthHeight), 0.0f);
+        for (int i = 0; i <= m_GridWidth; i++)
+        {
+            Debug.DrawLine(new Vector3((i * m_GridWidth * m_GridCellWidthHeight), 0.0f, 0.0f) + m_Offset, new Vector3((i * m_GridWidth * m_GridCellWidthHeight), m_GridHeight * m_GridCellWidthHeight, 0.0f) + m_Offset, Color.black);
+        }
+
+        for (int i = 0; i <= m_GridHeight; i++)
+        {
+            Debug.DrawLine(new Vector3(0.0f, (i * m_GridHeight * m_GridCellWidthHeight), 0.0f) + m_Offset, new Vector3(m_GridWidth * m_GridCellWidthHeight * m_GridCellWidthHeight, (i * m_GridHeight), 0.0f) + m_Offset, Color.black);
+        }
+
+    }
+
+    void GridSystem()
+    {
+        int xPos;
+        int yPos;
+        
+        for (int x = 0; x <= 20; x++)
+        {
+            for (int y = 0; y <= 20; y++)
+            {
+                xPos = -100 + (x * 20);
+                yPos = -100 + (y * 20);
+                GridList.Add( new Vector2(xPos, yPos) );
+            }
+        }
+
+        
+    }
+
+    void GridPosition(Transform ActiveCivilian)
+    {
+        
+    }
 }
