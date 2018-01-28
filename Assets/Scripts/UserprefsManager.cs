8021f9b8141f2cc7cdda27d3bb87c30740634ae6
MSI\Ludovic <ludovicaubut@hotmail.com>
2018-01-28 11:58:44 -0500

    Level difficulty incrementation

--- Assets/Scripts/UserprefsManager.cs
+++ Assets/Scripts/UserprefsManager.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UserprefsManager : MonoBehaviour {
+	private ColorChange cc;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		cc = GameObject.Find ("Scriptsbucket").GetComponent<ColorChange> ();
+		cc.m_DifficultyCurrentLevel = PlayerPrefs.GetInt ("Difficulty");
+		PlayerPrefs.SetInt ("CurrentGameDifficulty", PlayerPrefs.GetInt("Difficulty"));
+		PlayerPrefs.SetInt ("Difficulty", 0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void IncrementDifficulty() {
+		int difficulty = Mathf.Clamp (PlayerPrefs.GetInt ("CurrentGameDifficulty") + 1, 0, cc.m_DifficultyParameters.Count - 1);
+		PlayerPrefs.SetInt("Difficulty", difficulty);
 	}
 }
