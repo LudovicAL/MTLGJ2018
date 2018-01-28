e5074a4b0ee7a77441406b0bacfdd7f517afca6d
MSI\Ludovic <ludovicaubut@hotmail.com>
2018-01-28 10:27:30 -0500

    Kind of camera movement

--- Assets/Scripts/ControlsManager.cs
+++ Assets/Scripts/ControlsManager.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class ControlsManager : MonoBehaviour {
 	public bool wallConstructionMode;
-	public float cameraSpeed = 10.0f;
-	public float zoomSpeed = 3.0f;
+	public float cameraSpeed = 15.0f;
 	//public float updateGap = 0.02f;
-	public float fieldOfViewMin = 2.0f;
-	public float fieldOfViewMax = 20.0f;
+	private Vector3 poiterInitialPosition;
 	private float distanceBetweenFingers;
 	private LineRenderer hollowLine;
 	private float nextUpdateTime;
@@ -17,9 +15,6 @@ public class ControlsManager : MonoBehaviour {
 	private bool buildingWall;
 	private int screenWidth;
 	private int screenHeight;
-	private float startingZoomDistance;
-	private float endingZoomDistance;
-	private bool zoomingScreen;
 	private bool movingScreen;
 	private GameStatesManager gameStatesManager;	//Refers to the GameStateManager
 	private StaticData.AvailableGameStates gameState;	//Mimics the GameStateManager's gameState variable at all time
@@ -27,6 +22,7 @@ public class ControlsManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		poiterInitialPosition = Vector2.zero;
 		distanceBetweenFingers = 0.0f;
 		wallConstructionMode = false;
 		nextUpdateTime = 0.0f;
@@ -34,7 +30,6 @@ public class ControlsManager : MonoBehaviour {
 		coordList = new List<Vector2> ();
 		buildingWall = false;
 		movingScreen = false;
-		zoomingScreen = false;
 		screenWidth = Screen.width;
 		screenHeight = Screen.height;
 		hollowLine = GameObject.Find("Line").GetComponent<LineRenderer>();
@@ -51,88 +46,32 @@ public class ControlsManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (gameState == StaticData.AvailableGameStates.Playing) {
-			if (Application.isMobilePlatform) {	//ON MOBILE
+			//if (Application.isMobilePlatform) {	//ON MOBILE
 				if (Input.touchCount > 0) {	//USER HAS FINGER(S) ON
-					Debug.Log("Touches detected");
-					if (Input.touchCount == 1) {	//USER HAS 1 FINGER ON THE SCREEN
-						Debug.Log("1 touch detected");
-						if (wallConstructionMode) {
-							TouchWallConstructionController ();
-						} else {
-							TouchScreenMoveController ();
-						}
-					} else if (Input.touchCount == 2) { //USER HAS 2 FINGERS ON THE SCREEN
-						Debug.Log("2 touches detected");
-						TouchScreenZoomController();
-					} else {	//USER HAS TOO MANY FINGERS ON THE SCREEN
-						Debug.Log("Too many touches detected");
-						WallCanceled();
-						MovingScreenCanceled ();
-						ScreenZoomCanceled ();
+					if (wallConstructionMode) {
+						TouchWallConstructionController ();
+					} else {
+						TouchScreenMoveController ();
 					}
 				}
-			} else {	//ON PC
+			//} else {	//ON PC
+			/*
 				MouseClickController();
 				KeyboardButtonController ();
-			}
+			*/
+			//}
 			if (coordList.Count > 100) {
 				WallEnded (Input.mousePosition);
 			}
 		}
 	}
 
-	//Decides what to do with 2 fingers touch on the screen
-	public void TouchScreenZoomController() {
-		if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[1].phase == TouchPhase.Ended) {
-			Debug.Log("Screen zoom ended");
-			ScreenZoomEnded ();
-		} else if (Input.touches[0].phase == TouchPhase.Canceled || Input.touches[1].phase == TouchPhase.Canceled) {
-			Debug.Log("Screen zoom canceled");
-			ScreenZoomCanceled ();
-		} else if (Input.touches[0].phase == TouchPhase.Began) {
-			Debug.Log("Screen zoom began");
-			distanceBetweenFingers = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
-			ScreenZoomBegan ();
-		} else if (Input.touches[0].phase != TouchPhase.Stationary || Input.touches[1].phase != TouchPhase.Stationary) {
-			Debug.Log("Is it a screen zoom move or a screen zoom stationary?");
-			if (zoomingScreen)  {
-				Debug.Log("Screen zoom moved");
-				float newDistance = distanceBetweenFingers = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
-				float direction = newDistance - distanceBetweenFingers;
-				ScreenZoomMoved (Mathf.Sign(direction));
-				distanceBetweenFingers = newDistance;
-			}
-		}
-	}
-		
-	private void ScreenZoomBegan() {
-		zoomingScreen = true;
-	}
-
-	private void ScreenZoomMoved(float direction) {
-		if (zoomingScreen) {
-			Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView + direction, fieldOfViewMin, fieldOfViewMax);
-		}
-	}
-
-	private void ScreenZoomEnded() {
-		zoomingScreen = false;
-		distanceBetweenFingers = 0.0f;
-	}
-
-	private void ScreenZoomCanceled() {
-		zoomingScreen = false;
-		distanceBetweenFingers = 0.0f;
-	}
-
 	//Decides what to do with mouse clicks
 	public void MouseClickController() {
 		if (Input.GetMouseButton(0)) { //USER IS PRESSING THE MOUSE BUTTON
 			if (Input.GetMouseButtonDown(0)) { //USER PRESSED THE MOUSE BUTTON
-				Debug.Log ("WallBegan");
 				WallBegan (Input.mousePosition);
 			} else {	//USER JUST KEPT PRESSING THE MOUSE BUTTON
-				Debug.Log ("WallMoved");
 				WallMoved (Input.mousePosition);
 			}
 		} else if (Input.GetMouseButtonUp(0)) {	//USER RELEASED THE MOUSE BUTTON
@@ -142,23 +81,16 @@ public class ControlsManager : MonoBehaviour {
 
 	//Decides what to do with keyboard button clicks
 	public void KeyboardButtonController() {
-		if (Input.GetButton("Horizontal")) {	//USER IS PRESSING A HORIZONTAL ARROW KEY
-			MoveCameraHorizontally(Mathf.Sign(Input.GetAxisRaw("Horizontal")));
+		if (Input.GetMouseButtonDown(0)) {
+			poiterInitialPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		}
-		if (Input.GetButton("Vertical")) {	//USER IS PRESSING A VERTICAL ARROW KEY
-			MoveCameraVertically(Mathf.Sign(Input.GetAxisRaw("Vertical")));
+		if (Input.GetButton("Horizontal") ) {	//USER IS PRESSING A HORIZONTAL ARROW KEY
+			MoveScreenMagically(Camera.main.ScreenToWorldPoint (Input.mousePosition));
+		} else if (Input.GetButton("Vertical")) {	//USER IS PRESSING A VERTICAL ARROW KEY
+			MoveScreenMagically(Camera.main.ScreenToWorldPoint (Input.mousePosition));
 		}
 		if (Input.GetButton("Cancel")) {
 			Application.LoadLevel(Application.loadedLevel);
-		}
-		if (Input.GetButton("Fire1")) {
-			zoomingScreen = true;
-			ScreenZoomMoved (1.0f);
-			zoomingScreen = false;
-		} else if (Input.GetButton("Fire2")) {
-			zoomingScreen = true;
-			ScreenZoomMoved (-1.0f);
-			zoomingScreen = false;
 		}
 	}
 
@@ -210,26 +142,36 @@ public class ControlsManager : MonoBehaviour {
 
 	//Moves the camera horizontally
 	private void MoveCameraHorizontally(float direction) {
-		Vector2 vecDir = new Vector2 (direction, 0);
+		Vector2 vecDir = new Vector2 (-direction, 0);
 		cameraTransform.Translate(vecDir * Time.deltaTime * cameraSpeed);
 	}
 
 	//Moves the camera vertically
 	private void MoveCameraVertically(float direction) {
-		Vector2 vecDir = new Vector2 (0, direction);
+		Vector2 vecDir = new Vector2 (0, -direction);
 		cameraTransform.Translate(vecDir * Time.deltaTime * cameraSpeed);
+	}
+
+
+	public void MoveScreenMagically(Vector3 fingerPosition) {
+		Vector3 direction = fingerPosition - poiterInitialPosition;
+		Vector3 newPosition = Camera.main.ScreenToWorldPoint (direction);
+		newPosition.z = Camera.main.transform.position.z;
+		Camera.main.transform.position = newPosition;
 	}
 
 	//Player asked to move the screen in the current frame
 	private void MovingScreenBegan() {
+		poiterInitialPosition = Camera.main.ScreenToWorldPoint (Input.GetTouch (0).position);
 		movingScreen = true;
 	}
 
 	//Player kept asking to move the screen in the current frame
 	private void MovingScreenMoved() {
 		if (movingScreen) {
-			MoveCameraHorizontally (Mathf.Sign (Input.GetTouch(0).deltaPosition.x));
-			MoveCameraVertically (Mathf.Sign (Input.GetTouch(0).deltaPosition.y));
+			MoveScreenMagically (Input.GetTouch (0).position);
+			//MoveCameraHorizontally (Mathf.Sign (Input.GetTouch(0).deltaPosition.x));
+			//MoveCameraVertically (Mathf.Sign (Input.GetTouch(0).deltaPosition.y));
 		}
 	}
 
